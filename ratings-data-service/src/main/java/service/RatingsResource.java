613e7717ef39a24435e5ce635f50a07353dbb504
Blazeye <53467795+Blazeye@users.noreply.github.com>
2019-11-12 16:29:23 +0100

    Add files via upload

--- ratings-data-service/src/main/java/service/RatingsResource.java
+++ ratings-data-service/src/main/java/service/RatingsResource.java
@@ -32,8 +32,8 @@ public class RatingsResource {
     @RequestMapping("/users/{userId}")
     public UserRating getUserRating(@PathVariable("userId") String userId){
         List<Rating> ratings = Arrays.asList(
-            new Rating ( "1234", 4 ),
-            new Rating ( "5678", 3)
+            new Rating ( "3896198", 4 ),
+            new Rating ( "3916198", 3)
         );
         UserRating userRating = new UserRating();
         userRating.setUserRating(ratings);
